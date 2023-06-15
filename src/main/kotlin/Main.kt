ae38573b548ab19e20473ec347383fabfdaf3709
be.sotnikova <bsotnikova@mail.ru>
2023-06-15 16:25:16 +0400

    add the first work (edits)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,12 +14,10 @@ fun main() {
 //    println(map2)
 
 
-//    val number = listOf(89967877875, 87766754756, 87768764637, 89978746756)
     val phoneBook = mapOf("Viktor" to 89967877875, "Olga" to 87766754756, "Elena" to 87768764637, "Andrey" to 89978746756)
     val phoneBookSize = phoneBook.size
     val isBig = phoneBook.size > 10
     println(isBig)
-//    val isBig = phoneBookSize > 10
     println(phoneBook)
     println(phoneBook.keys)
     println(phoneBook.values)
