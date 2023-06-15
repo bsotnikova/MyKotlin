dbb728cb40cb0f994a3daebc5314125a9fb13617
be.sotnikova <bsotnikova@mail.ru>
2023-06-15 16:23:18 +0400

    add the first work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+//    val name = "Bella"
+//    val surname = "Sotnikova"
+//    val height: Int = 160
+//    height.toShort()
+//    val childHeight = 100
+//    val isChild = height > childHeight
+//    println("I am $name")
+//    val listBuy= listOf("milk", "eggs", "meat")
+//    val listCount= listOf(1,10,5)
+//    val map = mapOf("milk" to 1, "eggs" to 10)
+//    println(map["milk"])
+//    val map2 = listBuy.zip(listCount)
+//    println(map2)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+//    val number = listOf(89967877875, 87766754756, 87768764637, 89978746756)
+    val phoneBook = mapOf("Viktor" to 89967877875, "Olga" to 87766754756, "Elena" to 87768764637, "Andrey" to 89978746756)
+    val phoneBookSize = phoneBook.size
+    val isBig = phoneBook.size > 10
+    println(isBig)
+//    val isBig = phoneBookSize > 10
+    println(phoneBook)
+    println(phoneBook.keys)
+    println(phoneBook.values)
+
+
+
+
 }
+
+
