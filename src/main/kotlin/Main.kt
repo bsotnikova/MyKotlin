a85e13181a837d2708be62da4abf3d48745d7ac8
be.sotnikova <bsotnikova@mail.ru>
2023-06-23 17:49:17 +0400

    add the second work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,48 @@
 fun main() {
+    val string = "F2p)v\"y233{0->c}ttelciFc"
+    val stringLenght = string.length
+    val first = string.take(stringLenght / 2)
+    val second = string.takeLast(stringLenght / 2)
+    val firstString9 = encryptFirstHalf(first)
+    val secondString4 = encryptSecondHalf(second)
+    println(firstString9 + secondString4)
+
+
+}
+
+     fun encryptFirstHalf (first: String): String {
+         val firstString = first.map { char -> char + 1 }.joinToString("")
+         val firstString5 = firstString.replace("5", "s")
+         val firstString6 = firstString5.replace("4", "u")
+         val firstString7 = firstString6.map { char -> char - 3 }.joinToString("")
+         val firstString8 = firstString7.replace("0", "o")
+         return firstString8
+     }
+
+      fun encryptSecondHalf(second: String) : String {
+          val secondString = second.reversed()
+          val secondString2 = secondString.map { char -> char - 4 }.joinToString("")
+          val secondString3 = secondString2.replace("_", " ")
+          return secondString3
+      }
+
+
+
+
+
+//    val x = listOf<String>(f, 2, p, ), V, ", Y, 2, 3, 3, {, 0)
+//    val string = "F2p)v\"y233{0"
+//    val word = string.map { it.uppercaseChar() }
+//    println(word)
+//    val shiftedString = word.map {char -> char + 1}.joinToString("")
+//    println(shiftedString)
+//    val sourceString = mutableMapOf<Int, String>( 1 to "a", 2 to "b", 3 to "c")
+//    val separator = ", "
+//
+//    val str = input.joinToString(separator, transform = Int::toString)
+//
+//    println(str)
+
 //    val name = "Bella"
 //    val surname = "Sotnikova"
 //    val height: Int = 160
@@ -14,17 +58,28 @@ fun main() {
 //    println(map2)
 
 
-    val phoneBook = mapOf("Viktor" to 89967877875, "Olga" to 87766754756, "Elena" to 87768764637, "Andrey" to 89978746756)
-    val phoneBookSize = phoneBook.size
-    val isBig = phoneBook.size > 10
-    println(isBig)
-    println(phoneBook)
-    println(phoneBook.keys)
-    println(phoneBook.values)
+//    val phoneBook = mutableMapOf("Viktor" to 89967877875, "Olga" to 87766754756, "Elena" to 87768764637,
+//    "Andrey" to 89978746756)
+//    val phoneBookSize = phoneBook.size
+//    val isBig = phoneBook.size > 10
+//    println(isBig)
+//    println(phoneBook)
+//    println(phoneBook.keys)
+//    println(phoneBook.values)
+//    fun addContact(x: String, y: Long) {
+//    phoneBook[x] = y
+//}
+//    fun deleteContact (x: String = "Olga"){
+//        phoneBook.remove(x)
+//    }
+//    addContact("Olga", 38764739)
+//    println(phoneBook)
+//    deleteContact()
+//    println(phoneBook)
 
 
 
 
-}
+
 
 
